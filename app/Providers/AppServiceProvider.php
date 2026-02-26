ad7e534a92e628be6d5494bce3a5a72ea1f01adc
aditioirawan <aditioirawan535@gmail.com>
2026-02-27 05:34:52 +0700

    Auto migrate on boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Tambahkan blok ini untuk migrasi otomatis
+        if (!Schema::hasTable('migrations')) {
+            Artisan::call('migrate --force');
+        }
     }
 }
