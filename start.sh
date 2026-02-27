bfdaf9a0467dd336658ab83f91f02b7914b2941f
aditioirawan <aditioirawan535@gmail.com>
2026-02-27 10:30:12 +0700

    force fresh migrate

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-echo "Running Laravel migrations..."
-php artisan migrate --force
+echo "Clearing config..."
+php artisan config:clear
+php artisan cache:clear
+
+echo "Running fresh migration..."
+php artisan migrate:fresh --force
 
 echo "Starting Apache..."
 apache2-foreground
