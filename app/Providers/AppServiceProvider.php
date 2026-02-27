d0ec3a5475e6b7cc5977f54aaebb7d1f2f743d26
aditioirawan <aditioirawan535@gmail.com>
2026-02-27 07:12:48 +0700

    Disable auto-migration to fix connection error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,13 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Log ini akan muncul di dashboard Render jika kodenya jalan
-        Log::info('--- AppServiceProvider boot dijalankan ---');
-
-        // Cek apakah tabel migrasi sudah ada, kalau belum, jalankan migrasi
-        // Ini akan memaksa database terbuat otomatis saat website dibuka
-        if (!Schema::hasTable('migrations')) {
-            Artisan::call('migrate --force');
-        }
+        // Log ini saja yang kita simpan untuk memastikan kode terbaca
+        // Kita HAPUS perintah Artisan::call('migrate') agar aplikasi tidak crash
+        Log::info('--- AppServiceProvider boot dijalankan: Database migrasi dimatikan ---');
     }
 }
